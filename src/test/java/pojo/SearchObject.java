dfde2f4385e8d070ac2887c6647a83526346a7ff
Ivan Basenko <primeivan28@gmail.com>
2018-06-21 19:39:37 +0300

    Оптимизация запроса

--- src/test/java/pojo/SearchObject.java
+++ src/test/java/pojo/SearchObject.java
@@ -3,8 +3,6 @@ package pojo;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.util.List;
-
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SearchObject {
 
